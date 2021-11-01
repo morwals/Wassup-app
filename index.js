2fdf96520c832a713caa3e16fb8426121057b00f
morwals <88887916+morwals@users.noreply.github.com>
2021-11-01 23:20:16 +0530

    add proc

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const users={};
+
+app.use(express.static("public"));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on("new-user-joined",name=>{
+                 users[socket.id]=name;
+                socket.broadcast.emit("user-joined",name);
+             });
+        
+             socket.on("send",message=>{
+                 socket.broadcast.emit("receive",{message:message,name:users[socket.id]});
+             });
+
+             socket.on("disconnect",message=>{
+                socket.broadcast.emit("left",users[socket.id]);
+                delete users[socket.id];
+            });
+});
+
+
+let port=process.env.PORT;
+
+if(port==null || port==""){
+    port=3000;
+}
+
+server.listen(port, () => {
+    console.log('listening on *:3000');
+  });
+  
+
+
+
+
+
+
+
+// io.on("connection",socket=>{
+//    
+// });
+
+// server.listen(3000, () => {
+//   console.log('listening on *:3000');
+// });
+
+  
---
+++ public/client.js
@@ -0,0 +1,45 @@
+var socket = io();
+    
+const form=document.getElementById("frm");
+
+const messageInp=document.getElementById("msginp");
+
+const msgContainer=document.getElementById("container");
+
+var audio= new Audio('audio.wav');
+
+const append = (msg,position)=>{
+  const msgElement = document.createElement("div");
+  msgElement.innerText=msg;
+  msgElement.classList.add("msg");
+  msgElement.classList.add(position);
+  msgContainer.append(msgElement);
+
+  if(position==="left"){
+    audio.play();
+  }
+}
+
+form.addEventListener("submit",(e)=>{
+  e.preventDefault();
+  const m=messageInp.value;
+  append("you: "+m,"right");
+  socket.emit("send",m);
+  messageInp.value="";
+});
+
+const nam =prompt("Enter your username to start chat.");
+socket.emit("new-user-joined", nam);
+
+
+socket.on("user-joined",name=>{
+    append(name+" joined the chat.","right");
+});
+
+socket.on("receive",data=>{
+  append(data.name+" :"+data.message,"left");
+});
+
+socket.on("left",data=>{
+  append(data+" left the chat","left");
+});
